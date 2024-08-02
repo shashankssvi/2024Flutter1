fa60b84681158d8088e25c1951ad44264d79d720
shashankssvi <personal.shashank.s@gmail.com>
2024-08-02 21:35:27 +0530

    app1

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main(){
   runApp(Screen1());
@@ -6,20 +7,41 @@ void main(){
 
 class Screen1 extends StatelessWidget {
   const Screen1({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title:Center(
-            child: Text("cat"),
-          ),
+      home: Audioplay(),
+    );
+  }
+}
+
+class Audioplay extends StatefulWidget {
+  const Audioplay({super.key});
+  @override
+  State<Audioplay> createState() => _AudioplayState();
+}
+
+class _AudioplayState extends State<Audioplay> {
+  var audio = AudioPlayer();
+  playaudio()async{
+    await audio.play(AssetSource("cat1.mp3"));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Center(
+          child: Text("cat"),
         ),
-        body: Align(
-          alignment:Alignment.center,
+      ),
+      body: Align(
+        alignment:Alignment.center,
+        child: InkWell(
           child: Image.asset("assets/cat.jpg"),
+          onTap: () async{
+            await playaudio();
+          },
         ),
       ),
     );
